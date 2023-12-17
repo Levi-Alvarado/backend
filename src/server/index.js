70c170496a34b4b3a10d7872ff42ed7f07cd8fe4
Levi-Alvarado <levia1316@gmail.com>
2023-12-17 19:58:44 -0300

    commit 2 added cors

--- src/server/index.js
+++ src/server/index.js
@@ -6,7 +6,11 @@ import * as routes from './routes/index.js'
 const PORT = process.env.PORT ?? 3_000
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }))
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(routes.users)
